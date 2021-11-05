7aa0c36017c8cf68c544ceaade844ed63cd4a24d
TreatHunter <michael.zanin.a@gmail.com>
2021-11-05 19:43:39 +0300

    modified to use key that is set at the beggining of program

--- src/main/java/org_treathunter/java_university_7th_semester_ip_lab1_json_file_autontification_app/ecb/encryption/AES.java
+++ src/main/java/org_treathunter/java_university_7th_semester_ip_lab1_json_file_autontification_app/ecb/encryption/AES.java
@@ -31,12 +31,11 @@ public class AES {
             e.printStackTrace();
         }
     }
- 
-    public static String encrypt(String strToEncrypt, String secret) 
+
+    public static String encrypt(String strToEncrypt) 
     {
         try
         {
-            setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
@@ -48,11 +47,10 @@ public class AES {
         return null;
     }
  
-    public static String decrypt(String strToDecrypt, String secret) 
+    public static String decrypt(String strToDecrypt) 
     {
         try
         {
-            setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
@@ -62,5 +60,5 @@ public class AES {
             System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
-    }
+    }    
 }
